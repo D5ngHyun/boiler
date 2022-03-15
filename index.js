79e9e223c3022c396b8374adeeac38a0b4fa79f5
DongHyun Lee <dmadkr1541@naver.com>
2022-03-15 16:34:01 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+const config = require("./config/key");
 
 const mongoose = require("mongoose");
+const User = require("./models/User");
+
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 mongoose
-  .connect(
-    "mongodb+srv://dongs:ehdgus1541@boilerplate.tp2x3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      //   useCreateIndex: true,
-      //   useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+  })
   .then(() => {
     console.log("connect ...");
   })
@@ -22,4 +30,24 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port);
+app.post("/register", (req, res) => {
+  /**
+   * 회원 가입 할때 필요한 정보들을 client에서 가져오면
+   * 그것들을 데이터 베이스에 넣어준다.
+   */
+
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+
+    console.log("what? ");
+  });
+});
+
+app.listen(port, () => {
+  console.log("listening at port:", port);
+});
